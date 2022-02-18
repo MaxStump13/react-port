727193cecf85c7075c8a1078a6fbc45db8ce7e4c
MaxStump13 <stumpmaximilian@gmail.com>
2022-02-17 21:08:25 -0500

    Updates

---
+++ precache-manifest.5fd13ab299f497d303603efd4f0617e0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496b1625bb39ae31059b9ad27e37e9e1",
+    "url": "/react-port/index.html"
+  },
+  {
+    "revision": "b6985656e0fe2f2f8a61",
+    "url": "/react-port/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "286216d6c43c91e6b537",
+    "url": "/react-port/static/css/main.3f309470.chunk.css"
+  },
+  {
+    "revision": "b6985656e0fe2f2f8a61",
+    "url": "/react-port/static/js/2.48978509.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-port/static/js/2.48978509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f42438f8c4d0ed9b58dd",
+    "url": "/react-port/static/js/3.f8ec068e.chunk.js"
+  },
+  {
+    "revision": "286216d6c43c91e6b537",
+    "url": "/react-port/static/js/main.58b2311a.chunk.js"
+  },
+  {
+    "revision": "23e3eb5a7865a1d2b446",
+    "url": "/react-port/static/js/runtime-main.7b3accc2.js"
+  },
+  {
+    "revision": "176ea0e3e7674706bdda77d66f9e4493",
+    "url": "/react-port/static/media/MaxStumpWebDevResume.176ea0e3.pdf"
+  },
+  {
+    "revision": "818cc73233124a37989ed2a91d89c862",
+    "url": "/react-port/static/media/profile-picture.818cc732.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-port/precache-manifest.d5e6aa32301b867cef5cedc97048e208.js"
+  "/react-port/precache-manifest.5fd13ab299f497d303603efd4f0617e0.js"
 );
 
 self.addEventListener('message', (event) => {
